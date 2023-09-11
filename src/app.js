d2b5fabbf58bfcf47d927ca462767ee66c26df51
Vince489 <vincentcornelius489@gmail.com>
2023-09-10 22:21:09 -0400

    cors

--- src/app.js
+++ src/app.js
@@ -10,10 +10,16 @@ const cookieParser = require("cookie-parser");
 // create server app
 const app = express();
 
-// app.use(cors({ origin: '*' }));
+const corsOptions = {
+  origin: 'https://planetvirtron.com/',
+  credentials: true, // Allow credentials (cookies)
+};
+
+app.use(cors(corsOptions));
 
 
-app.use(cors());
+
+// app.use(cors());
 app.use(express.json())
 app.use("/api/v1", routes);
 app.use(cookieParser());
